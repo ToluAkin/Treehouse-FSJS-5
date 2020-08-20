e4ca0b87a409309ae649da3f6b87a8bcc17d4d07
Toluwalase <toluakintoye@gmail.com>
2020-08-20 22:27:37 +0100

    birthday and phone number formatted

--- public_api_request-v1/js/scripts.js
+++ public_api_request-v1/js/scripts.js
@@ -12,7 +12,7 @@ function fetchData(url) {
             .catch(error => console.log('Looks like there was a problem', error))
 }
 
-fetchData('https://randomuser.me/api/?results=12')
+fetchData('https://randomuser.me/api/?results=12&?nat=us')
     .then(employees => generateCard(employees.results))
     // .then(data => console.log((data.results)))
 
@@ -33,6 +33,9 @@ function checkStatus(response) {
  * @param {*} employees 
  */
 function generateCard(employees) {
+    if (gallery === '') {
+        gallery.textContent = 'Loading.....'
+    }
     const card = employees.map(employee => `
     <div class='card'>
         <div class='card-img-container'>
@@ -41,17 +44,16 @@ function generateCard(employees) {
         <div class="card-info-container">
             <h3 id="name" class="card-name cap">${employee.name.first}, ${employee.name.last}</h3>
             <p class="card-text">${employee.email}</p>
-            <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
+            <p class="card-text cap">${employee.location.city}</p>
         </div>
     </div>
     `).join('');
     gallery.innerHTML = card;
 
     const cards = document.querySelectorAll('.card');
-    console.log(cards);
+
     for (let i = 0; i < cards.length; i++) {
         cards[i].addEventListener('click', (e) => {
-            console.log(e.target );
             if (cards[i].contains(e.target)) {
                 generateModal(employees[i])
             }
@@ -64,19 +66,23 @@ function generateCard(employees) {
  * @param {*} employee 
  */
 function generateModal(employee) {
+    let getDate = new Date(employee.dob.date);
+    let phoneDigits = employee.cell.replace(/[^\d]/g, "");
+    let formattedPhoneNumber = phoneDigits.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3")
+
     const modal = `
         <div class="modal-container">
             <div class="modal">
                 <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                 <div class="modal-info-container">
-                    <img class="modal-img" src="${employee.picture.medium}" alt="profile picture">
+                    <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
                     <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
                     <p class="modal-text">${employee.email}</p>
                     <p class="modal-text cap">${employee.location.city}</p>
                     <hr>
-                    <p class="modal-text">${employee.cell}</p>
-                    <p class="modal-text">${employee.location.street.number} ${employee.location.street.name}., ${employee.country} OR ${employee.location.postcode}</p>
-                    <p class="modal-text">Birthday: ${employee.dob.date}</p>
+                    <p class="modal-text">${formattedPhoneNumber}</p>
+                    <p class="modal-text">${employee.location.street.number} ${employee.location.street.name}, ${employee.nat} ${employee.location.postcode}</p>
+                    <p class="modal-text">Birthday: ${getDate.toLocaleDateString()}</p>
                 </div>
             </div>
 
